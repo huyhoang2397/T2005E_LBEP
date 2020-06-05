6f38b8cf8800778e89e00ec12e7c51a0710ce1b7
huyhoang2397 <huyhoang2397@gmail.com>
2020-06-05 18:34:19 +0700

    Nop bai buoi 2

--- Session2_ab.cpp
+++ Session2_ab.cpp
@@ -5,5 +5,12 @@ int main(){
 	printf("Nhap hai so nguyen a va b:");
 	printf("\na = "),scanf("%d",&a);
 	printf("b = "),scanf("%d",&b);
+	if(a>=b){
+		if(b==0){
+			printf("\nKhong thuc hien duoc phep %d/%d",a,b);
+		}else{
+			printf("\nKet qua %d/%d = %.3f",a,b,(float)a/b);
+		}
+	}
 	else printf("Ket qua %d*%d = %d",a,b,a*b);
 }
