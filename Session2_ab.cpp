0f9123fa937fc2eafe1f352e93a8f13a4f129c0d
huyhoang2397 <huyhoang2397@gmail.com>
2020-06-05 18:27:29 +0700

    Update Session2_ab.cpp

--- Session2_ab.cpp
+++ Session2_ab.cpp
@@ -5,6 +5,5 @@ int main(){
 	printf("Nhap hai so nguyen a va b:");
 	printf("\na = "),scanf("%d",&a);
 	printf("b = "),scanf("%d",&b);
-	if(a>=b)printf("\nKet qua %d/%d = %.3f",a,b,(float)a/b);
 	else printf("Ket qua %d*%d = %d",a,b,a*b);
 }
